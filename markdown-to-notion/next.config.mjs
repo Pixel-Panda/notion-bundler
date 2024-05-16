43aabec109949a17a781d8bc651585ab0f9ad4f8
Drew Andersen <drew@redpanda.work>
2024-05-16 18:38:01 +0000

    new

--- markdown-to-notion/next.config.mjs
+++ markdown-to-notion/next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
 export default nextConfig;
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  webpack(config, options) {
+    config.resolve.modules.push(__dirname);
+    return config;
+  },
+}
